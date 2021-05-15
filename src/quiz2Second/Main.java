6a997b13b65dfac997587cce32ed163c23591357
djack11 <djack11@hotmail.com>
2021-05-15 02:55:09 -0400

    quiz2again

---
+++ src/quiz2Second/Main.java
@@ -0,0 +1,83 @@
+package quiz2Second;
+
+import java.util.Scanner;
+
+    
+
+public abstract class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Hello! Please enter a grade:");
+		int grade = scanner.nextInt();	
+	System.out.println("Would you like to enter another grade? Y/N:");
+	grade = scanner.nextInt();
+	String Y, y;
+	String N, n;
+	String highestGrade;
+	String lowestGrade;
+	String averageGrade; 
+	
+	
+	if (grade = N); 
+		System.out.println(highestGrade + " get highest grade ");        
+		System.out.println(lowestGrade + " get lowest grade ");     
+		System.out.println(averageGrade + " get average grade "); 
+	
+	
+	else (grade = Y)
+	
+	String aGrade;
+	String bGrade;
+	String cGrade;
+	String dGrade;
+	
+	
+	System.out.println("A Students: "+aGrade );	
+	System.out.println("B Students: "+bGrade );	
+	System.out.println("C Students: "+cGrade );	
+	System.out.println("Failed Students:"+dGrade );
+	
+	if (grade>=90) {
+		grade = 'A';	
+	}else if(grade>=80 &&  grade<= 89 ) {
+		grade = 'B';
+	}
+	else if (grade>=70 &&  grade<= 79) {
+		grade = 'C';
+	}
+	else {
+	        grade = 'D';
+	}
+	     
+	{
+	
+	}
+	
+	
+
+	
+
+
+
+
+	
+	
+
+		
+		
+	
+		
+		   
+		
+		
+		
+		
+		
+        
+		
+		 
+
+	}
+
+}
